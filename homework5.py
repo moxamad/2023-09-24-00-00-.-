cb87d447ecf17e41c2d412cddcb2df778842604e
moxamad <R8AGn5VhFSy7Rz@yandex.ru>
2024-06-29 01:44:00 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,6 @@
+immutable_var=1,'рик',True
+#immutable_var[0]=200: не поддерживает обращения по илиментам
+print(immutable_var)
+mutable_list=[1,'xxx',True]
+mutable_list[1]='морти'
+print(mutable_list)
